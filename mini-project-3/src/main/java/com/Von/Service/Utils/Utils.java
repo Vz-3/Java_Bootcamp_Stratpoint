4ebc682e412d96eca76aa5f9bb37aa14bbe5bdf3
Vz-3 <>
2024-06-30 02:30:02 +0800

    feat: load & save products using json

--- mini-project-3/src/main/java/com/Von/Service/Utils/Utils.java
+++ mini-project-3/src/main/java/com/Von/Service/Utils/Utils.java
@@ -1,10 +1,21 @@
 package com.Von.Service.Utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import com.Von.Model.Catalog.Product;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +23,8 @@ public class Utils {
     private static final Scanner scn = new Scanner(System.in);
     private static final Integer defaultMinLength = 4;
     private static final Integer defaultMaxLength = 20;
-    private static final Integer defaultMaxPrice = 9_999_999;
     private static final Logger logger = LoggerFactory.getLogger(Utils.class);
-
+    private static final String fileName = "src/main/resources/data.products.json";
     private Utils() {}
 
     private static String validator(String fieldName, Integer minLength, Integer maxLength) {
@@ -66,4 +76,65 @@ public class Utils {
     public static String validateStringInput(String fieldName, Integer minLength, Integer maxLength) {
         return validator(fieldName, minLength, maxLength);
     }
+
+    public static Map<String, Product> importData() {
+        Map<String, Product> existingCatalog = new HashMap<>();
+
+        try {
+            String jsonContent = FileUtils.readFileToString(new File(fileName), StandardCharsets.UTF_8);
+            JSONArray jsonArray = new JSONArray(jsonContent);
+
+            jsonArray.forEach(jsonObj -> {
+                JSONObject jsonObject = (JSONObject) jsonObj;
+                String sn = jsonObject.getString("sn");
+                String name = jsonObject.getString("product_name");
+                BigDecimal price = jsonObject.getBigDecimal("product_price");
+                String seller = jsonObject.getString("product_seller");
+                String description = jsonObject.getString("product_description");
+                BigDecimal rating = jsonObject.getBigDecimal("product_rating");
+
+                Product existingProduct = new Product.ProductBuilder(name, price)
+                        .seller(seller).description(description).rating(rating)
+                        .build();
+
+                existingCatalog.put(sn, existingProduct);
+            });
+        } catch (IOException | ClassCastException e) {
+            logger.error("Utils.importData error: ", e);
+        }
+        return existingCatalog;
+    }
+
+
+    public static boolean storeData(Map<String, Product> catalog) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            StringBuilder json = new StringBuilder("[\n");
+            catalog.forEach((key, product) -> json.append(String.format("""
+            {
+                "sn": "%s",
+                "product_name": "%s",
+                "product_price": "%s",
+                "product_seller": "%s",
+                "product_description": "%s",
+                "product_rating": "%s"
+            },
+        """,
+                    key,
+                    product.getProductName(),
+                    product.getProductPrice(),
+                    product.getProductSeller() == null ? "" : product.getProductSeller(),
+                    product.getProductDescription() == null ? "" : product.getProductDescription(),
+                    product.getProductCompoundedRatings())));
+            json.deleteCharAt(json.length() - 2); // Remove the trailing comma and newline
+            json.append("]");
+
+            writer.write(json.toString());
+            System.out.println("Data written to " + fileName);
+        } catch (IOException e) {
+            logger.error("Utils.storeData error: ", e);
+            return false;
+        }
+        return true;
+    }
+
 }
